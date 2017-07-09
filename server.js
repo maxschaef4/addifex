9abc0402df1e27e9db71624c08e4b448c4074e5f
maxschaef4 <maxschaef31@gmail.com>
2017-07-09 15:58:43 -0500

    Added QA testing to the site

--- server.js
+++ server.js
@@ -17,29 +17,35 @@ app.use(express.static(__dirname + '/public'));
 
 app.set('port', process.env.PORT || 8081);
 
+app.use(function(req, res, next){
+    res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+    
+    next();
+});
+
 //Home page
 app.get('/', function(req, res){
     res.render('index');
-})
+});
 
 //About Page
 app.get('/about', function(req, res){
     res.render('about', {scooby : scooby.getScooby()});
-})
+});
 
 //404 page
 app.use(function(req, res){
     res.status(404);
     res.render('404');
-})
+});
 
 //500 page
 app.use(function(err, req, res, next){
     console.error(err.stack);
     res.status(500);
     res.render('500');
-})
+});
 
 app.listen(app.get('port'), function(){
     console.log('Express started on http://127.0.0.1:' + app.get('port'));
-})
+});
